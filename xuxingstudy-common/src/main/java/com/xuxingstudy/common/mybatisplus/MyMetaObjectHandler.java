4f7b98ea69c2d5afa826218b6a22d435065830b1
Xuxing <104206034@qq.com>
2021-08-17 16:22:21 +0800

    no msg

---
+++ xuxingstudy-common/src/main/java/com/xuxingstudy/common/mybatisplus/MyMetaObjectHandler.java
@@ -0,0 +1,28 @@
+package com.xuxingstudy.common.mybatisplus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author xhb
+ * @since 2021/8/10
+ */
+@Slf4j
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐)
+    }
+}
